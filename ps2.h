afe806f3c39f0bf39fd71965d118839269091717
Pytheon <pawelecfilip6@gmail.com>
2020-12-13 20:22:44 +0100

    Added project files.
    Heather file for PS/2 communication protocole.

---
+++ ps2.h
@@ -0,0 +1,27 @@
+/*****************************************************************************************************
+*This file is a part of TM2 project which goal is to create a musical instrument.*
+******************************************************************************************************/
+/**
+ * @file ps2.h
+ * @author Pawelec, Skomiał
+ * @date Dec 2020
+ * @brief File containing enums, structures and declarations for LEDs.
+ * @ver 0.1
+ */
+
+#ifndef
+#define PS2_H
+
+/**
+ * @brief Initialise communication with PS/2 keyboard
+ */
+void Ps2_comm_init(void);
+
+/**
+	* @brief Interrupt handler for communication (to be decided)
+	*/
+void PORTA_IRQHandler(void);
+// If I understand correctly, there is no need to use timer module unless the host wants to transmit sth to keyboard
+
+
+#endif /* PS2_H */
