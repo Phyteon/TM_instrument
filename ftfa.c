4327efd9fb4cafdacc774314f976e202a8f5196b
KrzSko20 <krzysztof.skomial@gmail.com>
2020-12-16 12:58:35 +0100

    Added flash command sequence to read function

--- ftfa.c
+++ ftfa.c
@@ -12,17 +12,17 @@
  #include "ftfa.h"
  
 void Read_tone(void){
+
+	FTFA->FSTAT |= FTFA_FSTAT_CCIF(0);	// Flash command in progress
+		
+	if (FTFA->FSTAT != 0){			// Access error or protection detected from previous commands
+		FTFA->FSTAT = 0x30;	
+	}
+	
+	// Todo: FCCOB registers communication (as suggested in 27.4.10)
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	FTFA->FSTAT |= FTFA_FSTAT_CCIF(1);	// Flash command is completed
+	FTFA->FSTAT = 0x80;		// Flash command is completed, set no errors
 	
 }
  
