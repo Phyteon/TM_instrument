f2a32114ab150dcdd2dfb639b5f3abcef9498fa3
KrzSko20 <krzysztof.skomial@gmail.com>
2021-01-20 13:03:47 +0100

    Added new branch to optimize sounds
    Error in copying branch, needs to be repaired

--- Objects/dsp.d
+++ Objects/dsp.d
@@ -1,11 +1,11 @@
 ./objects/dsp.o: dsp.c dsp.h dat_prep.h const_dat_file.h \
-  D:\Tools\Keil_u_vision\Pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h \
-  D:\Tools\Keil_u_vision\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm0plus.h \
-  D:\Tools\Keil_u_vision\Core\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\Tools\Keil_u_vision\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
-  D:\Tools\Keil_u_vision\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
-  D:\Tools\Keil_u_vision\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
-  D:\Tools\Keil_u_vision\Core\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  D:\Tools\Keil_u_vision\Core\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  D:\Tools\Keil_u_vision\Pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h \
+  D:\Programy\Keil\Pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h \
+  D:\Programy\Keil\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm0plus.h \
+  D:\Programy\Keil\Core\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\Programy\Keil\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Programy\Keil\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Programy\Keil\Pack\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\Programy\Keil\Core\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  D:\Programy\Keil\Core\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  D:\Programy\Keil\Pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h \
   ps2.h dac.h
